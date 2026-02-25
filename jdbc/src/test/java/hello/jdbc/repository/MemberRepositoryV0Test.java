d3759fd7faaaef871d7151411a94d08b085d4a12
kejee1917 <kejee1917@gmail.com>
2026-02-25 23:13:46 +0900

    jdbc - row level

---
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,31 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException {
+        //save
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+
+        //find
+        Member byId = repository.findById(member.getMemberId());
+        log.info("findMember = {}", byId);
+
+        Assertions.assertThat(byId).isEqualTo(member);
+    }
+
+
+}
