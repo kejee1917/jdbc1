4bacc6608c94d6fcf2d2a96fc830a0af9e710e2a
kejee1917 <kejee1917@gmail.com>
2026-02-27 14:09:26 +0900

    transaction - Spring AOP 1

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -0,0 +1,47 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.SQLException;
+
+//@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceV3_3 {
+
+//    private final DataSource dataSource;
+//    private final PlatformTransactionManager transactionManager;
+//    private final TransactionTemplate txTemplate;
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 성공하면 커밋, 런타임(언체크)예외 터지면 롤백
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        // 비즈니스 로직
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney()- money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney()+ money);
+    }
+
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
